0539213b317b1b779ca0ffa600d36b539de4ba44
Yogini-Kanakala <85878318+Yogini-Kanakala@users.noreply.github.com>
2025-07-16 23:13:41 +0530

    week4 day 3

--- challange4/sol-1.sql
+++ challange4/sol-1.sql
@@ -113,3 +113,95 @@ FROM running_data
 WHERE rn = 1
 ORDER BY customer_id, txn_month;
 
+
+
+-- What is the closing balance for each customer at the end of the month?
+-- What is the percentage of customers who increase their closing balance by more than 5%?
+WITH running_data AS (
+  SELECT customer_id,
+    to_char(txn_date, 'YYYY-MM') AS txn_month,
+    txn_date,
+    -- cumulative running balance per customer over time
+    SUM(
+      CASE
+        WHEN txn_type = 'deposit'                THEN  txn_amount
+        WHEN txn_type IN ('withdrawal','purchase') THEN -txn_amount
+        ELSE 0
+      END
+    ) OVER (
+      PARTITION BY customer_id
+      ORDER BY   txn_date
+      ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS running_balance,
+    -- identify the last txn in each customer/month
+    ROW_NUMBER() OVER (
+      PARTITION BY customer_id, to_char(txn_date, 'YYYY-MM')
+      ORDER BY   txn_date DESC
+    ) AS rn
+  FROM customer_transactions
+),
+customer_data as(
+SELECT
+  customer_id,
+  txn_month           AS month,
+  running_balance     AS closing_balance,
+  case when running_balance is null  or  running_balance =0 then null 
+  else 
+  (lead(running_balance) over(partition by  customer_id) - running_balance  )*100.0/ running_balance 
+  end  as percentage
+FROM running_data
+WHERE rn = 1
+ORDER BY customer_id, txn_month
+)
+
+select  count( distinct case when percentage >5 then customer_id  end)*100.0/ count(distinct customer_id)
+from customer_data
+where percentage is not null 
+
+
+-- C. Data Allocation Challenge
+-- To test out a few different hypotheses - the Data Bank team wants to run an experiment where different groups of customers would be allocated data using 3 different options:
+
+-- Option 1: data is allocated based off the amount of money at the end of the previous month
+-- Option 2: data is allocated on the average amount of money kept in the account in the previous 30 days
+-- Option 3: data is updated real-time
+-- For this multi-part challenge question - you have been requested to generate the following data elements to help the Data Bank team estimate how much data will need to be provisioned for each option:
+
+-- running customer balance column that includes the impact each transaction
+-- customer balance at the end of each month
+-- minimum, average and maximum values of the running balance for each customer
+-- Using all of the data available - how much data would have been required for each option on a monthly basis?
+
+WITH data AS (
+  SELECT 
+    customer_id,
+    TO_CHAR(txn_date, 'YYYY-MM') AS txn_month,
+    txn_date,
+    SUM(
+      CASE
+        WHEN txn_type = 'deposit' THEN txn_amount
+        WHEN txn_type IN ('withdrawal', 'purchase') THEN - txn_amount
+        ELSE 0
+      END
+    ) OVER (
+      PARTITION BY customer_id
+      ORDER BY txn_date
+      ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS running_balance
+  FROM customer_transactions
+)
+SELECT 
+  customer_id, 
+  txn_month,
+  running_balance,
+  LAST_VALUE(running_balance) OVER (
+    PARTITION BY customer_id, txn_month
+    ORDER BY txn_date
+    ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+  ) AS month_end_balance,
+  MIN(running_balance) OVER (PARTITION BY customer_id, txn_month) AS min_end_balance,
+  MAX(running_balance) OVER (PARTITION BY customer_id, txn_month) AS max_end_balance,
+  AVG(running_balance) OVER (PARTITION BY customer_id, txn_month) AS avg_end_balance
+FROM data
+ORDER BY customer_id, txn_month;
+
